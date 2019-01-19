8e7483d4c984094ee0cb2bd6b8054401a92d1adb
nopparat231 <23.noop@gmail.com>
2019-01-19 17:01:12 +0700

    เพิ่มระบบหลังบ้าน

--- admin/index.php
+++ admin/index.php
@@ -1,20 +1,18 @@
-<?php session_start();?>
+
 <?php 
 
-if (!$_SESSION["UserID"]){  //check session
-
-	  Header("Location: ../index.php"); //ไม่พบผู้ใช้กระโดดกลับไปหน้า login form 
-
-}else{?>
+?>
 <!doctype html>
 <html>
 <head>
-<meta charset="UTF-8">
-<title>Admin page</title>
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" type="text/css">
+  <link rel="stylesheet" href="https://static.pingendo.com/bootstrap/bootstrap-4.1.3.css">
 </head>
+<?php include 'navbar.php'; ?>
 <body>
+
     <h1>You are Administrator </h1>
     <p><strong>hi</strong> :&nbsp;<?php print_r($_SESSION);?> //show detail login
       <?php //session_destroy();?>
@@ -25,4 +23,8 @@ if (!$_SESSION["UserID"]){  //check session
 
 </body>
 </html>
-<?php }?>
+<?php ?>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
---
+++ admin/navbar.php
@@ -0,0 +1,76 @@
+<?php session_start();?>
+ <nav class="navbar navbar-expand-md bg-dark navbar-dark">
+  <div class="container"> 
+    <button class="navbar-toggler navbar-toggler-right border-0" type="button" data-toggle="collapse" data-target="#navbar12">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbar12"> 
+      <a class="navbar-brand d-none d-md-block" href="index.php">
+        <i class="fa d-inline fa-lg fa-circle"></i>
+        <b>Home</b>
+      </a>
+
+      <?php if (isset($_SESSION["Userlevel"]) == "A") { ?>
+        <?php $user_id=$_SESSION['UserID']; ?>
+        <ul class="navbar-nav mx-auto">
+          <li class="nav-item"> 
+            <a class="nav-link" href="scoreall.php">คะแนนผู้ใช้งานทั้งหมด</a> </li>
+           <a class="nav-link" href="showchoice.php">คำถามทั้งหมด</a> </li>
+           <a class="nav-link" href="scoreall.php">คะแนนผู้ใช้งานทั้งหมด</a> </li>
+           <a class="nav-link" href="scoreall.php">คะแนนผู้ใช้งานทั้งหมด</a> </li>
+          </ul>
+
+        <?php }else{ ?>
+
+         <ul class="navbar-nav mx-auto">
+          <li class="nav-item"> 
+            <a class="nav-link" href="#"></a> </li>
+            <li class="nav-item dropdown"> 
+
+            </li>
+          </ul>
+
+        <?php }?>
+
+
+
+        <ul class="navbar-nav ">
+          <li class="nav-item "> 
+            <?php if (isset($_SESSION["Userlevel"]) == "A") { 
+
+              echo "<b class='fa text-primary nav-link' >".$_SESSION["User"]."</b>";
+            }else{ ?>
+
+
+
+            <?php    }
+
+            ?>
+
+          </li>
+          <li class="nav-item">  
+            <?php
+
+            if (isset($_SESSION["Userlevel"]) == "A") {
+              echo "<a class='fa text-danger nav-link' href='logout.php'>ออกจากระบบ</a>";
+
+            }else{?>
+
+              <form class="form-inline" action="login_db.php"> 
+                <input class="form-control mr-sm-2" type="text" placeholder="ชื่อผู้ใช้" id="Username" name="Username" required="required">
+                <input class="form-control mr-sm-2" type="Password" placeholder="รหัสผ่าน" id="Password"  name="Password" required="required"> 
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">เข้าสู่ระบบ</button>
+                <a class='nav-link text-lite' href='index.php.php' data-toggle='modal' data-target='#exampleModal'>ลืมรหัสผ่าน</a>
+                <!-- <a href="resetpassword.php" type="submit" class="form-control mr-sm-2">ลืมรหัสผ่าน</a> -->
+              </form>
+
+              <?php
+            }
+            ?> 
+
+          </li>
+          <?php //include 'resetpassword.php'; ?>
+        </ul>
+      </div>
+    </div>
+  </nav>
