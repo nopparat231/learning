64313c3a9534cefa28d4152ee3c746b99331c6f9
nopparat231 <23.noop@gmail.com>
2019-04-09 21:56:31 +0700

    fix ans

--- answer.php
+++ answer.php
@@ -4,33 +4,44 @@
 <?php
 
 
+for ($i=1; $i < 21; $i++) { 
 
 
-for ($i=1; $i < 20; $i++) { 
-  $a = 1;
 
-  while($result=mysqli_fetch_array($db_query))
+  $id = $_REQUEST["id$i"];
+
+
+  $sqla="SELECT * From testing WHERE id =".$id;
+  $db_querya=mysqli_query($con,$sqla) or die(mysqli_error());
+  //$resulta=mysqli_fetch_array($db_querya);
+
+  while($resulta=mysqli_fetch_array($db_querya))
   {
 
-    $an = $_REQUEST["answer$a"];
+    $an = $_REQUEST["answer$i"];
     $cn = $_REQUEST["c$i"];
 
-    echo "<h3>" . $a . " )  ".$result['question']."</h3>";
+    echo "<h3>" . $i . " )  ".$resulta['question']."</h3>";
 
 
-    $cnn = $result["c$cn"];
+    $cnn = $resulta["c$cn"];
 
     ?>
 
+    <h3>
+
+      <?php  echo "คำตอบของคุณ :   ".$cnn; ?>
+
+    </h3>
+
     <?php  if($cn == $an) {
-      echo "<h3> คำตอบของคุณ :   " . $cnn; "</h3><br>";
       echo "<h4>ผลลัพธ์ :<u> ถูก </u> </h4>";
     }elseif ($cn <> $an) {
       echo "<h4>ผลลัพธ์ :<u> ผิด </u> </h4>";
     }
     echo "<hr>";
 
-    $a++;
+   
   }
 
 }
