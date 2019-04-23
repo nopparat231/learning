945daaa73d7da256cacd177da3f5d87947034181
nopparat231 <23.noop@gmail.com>
2019-04-23 18:04:28 +0700

    test up

--- admin/index.php
+++ admin/index.php
@@ -14,7 +14,7 @@ if(session_status() == PHP_SESSION_NONE){
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" type="text/css">
+  <link rel="stylesheet" href="./css/font-awesome.min.css" type="text/css">
   <link rel="stylesheet" href="https://static.pingendo.com/bootstrap/bootstrap-4.1.3.css">
 </head>
 <?php include 'navbar.php'; ?>
